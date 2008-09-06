927f52885600f0f1ed70a744c7e853c4daf8c2de
sekelsenmat <sekelsenmat@985f7b8f-303b-0410-a9cd-c9b14c94f7dd>
2008-09-06 20:26:38 +0000

    improves db code

--- dbcomponents.pas
+++ dbcomponents.pas
@@ -23,6 +23,7 @@ type
     function  GetHeight(AID: Integer): Integer;
     function  GetPins(AID: Integer): Integer;
     function  GetWidth(AID: Integer): Integer;
+    procedure GoToRec(AID: Integer);
   end;
 
 var
@@ -51,7 +52,7 @@ begin
   FDataset.FieldDefs.Add('DRAWINGCODE', ftString);
 
   FDataset.Delimiter := ',';
-//  FDataset.FirstLineAsSchema := True;
+  FDataset.FirstLineAsSchema := True;
 
   FDataset.Active := True;
 end;
@@ -70,36 +71,71 @@ var
 begin
   AStringList.Clear;
 
-  for i := 1 to FDataset.RecordCount do
+{  for i := 1 to FDataset.RecordCount do
   begin
     FDataset.RecNo := i;
     CurField := FDataset.FieldByName(STR_DB_COMPONENTS_NAMEEN);
     AStringList.Add(CurField.Value);
+  end;}
+
+  CurField := FDataset.FieldByName(STR_DB_COMPONENTS_NAMEEN);
+  FDataset.First;
+  while not FDataset.EOF do
+  begin
+    AStringList.Add(CurField.Value);
+    FDataset.CursorPosChanged;
+    FDataset.Next;
   end;
 end;
 
 function TComponentsDatabase.GetDrawingCode(AID: Integer): string;
 begin
-  FDataset.RecNo := AID;
+  GoToRec(AID);
   Result := FDataset.FieldByName(STR_DB_COMPONENTS_DRAWINGCODE).Value;
 end;
 
 function TComponentsDatabase.GetHeight(AID: Integer): Integer;
 begin
-  FDataset.RecNo := AID;
+  GoToRec(AID);
   Result := StrToInt(FDataset.FieldByName(STR_DB_COMPONENTS_HEIGHT).Value);
 end;
 
 function TComponentsDatabase.GetPins(AID: Integer): Integer;
 begin
-  FDataset.RecNo := AID;
+  GoToRec(AID);
   Result := StrToInt(FDataset.FieldByName(STR_DB_COMPONENTS_PINS).Value);
 end;
 
 function TComponentsDatabase.GetWidth(AID: Integer): Integer;
 begin
-  FDataset.RecNo := AID;
+  GoToRec(AID);
   Result := StrToInt(FDataset.FieldByName(STR_DB_COMPONENTS_WIDTH).Value);
+end;
+
+{
+  Moves to the desired record using TDataset.Next and TDataset.Prior
+  Avoids using TDataset.RecNo which doesn't work in all datasets
+}
+procedure TComponentsDatabase.GoToRec(AID: Integer);
+begin
+  // We are before the desired record, move forward
+  if AID < FDataset.RecNo then
+  begin
+    while (not FDataset.EOF) and (AID < FDataset.RecNo) do
+    begin
+      FDataset.Next;
+      FDataset.CursorPosChanged;
+    end;
+  end
+  // We are after the desired record, move back
+  else if AID > FDataset.RecNo then
+  begin
+    while (AID < FDataset.RecNo) do
+    begin
+      FDataset.Next;
+      FDataset.CursorPosChanged;
+    end;
+  end;
 end;
 
 initialization
